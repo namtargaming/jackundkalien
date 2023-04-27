7183162c0ad400e26301bd5dc90c595f4ae1b235
Vitamoon <91440012+Vitamoon@users.noreply.github.com>
2023-04-27 15:08:39 -0700

    lhgjkh

--- game test/main.cpp
+++ game test/main.cpp
@@ -15,7 +15,7 @@ int main() {
         cout << "Welcome to d.tech Edventure! You are Ed...ward." << endl << "You see a small 9th grade child at a school. This is what they look like:" << endl;
         Enemy ninthGrader(10,10);
         ninthGrader.pepper();
-        cout << "Attack them" << endl ;
+        cout << endl << "Attack them" << endl ;
 
         mainGame.attackLoop(player, ninthGrader);
         
