3f72dc5db088473311377e6b99c55144a3b38fb9
Vitamoon <91440012+Vitamoon@users.noreply.github.com>
2023-04-28 12:51:09 -0700

    sdfsdfsdfa

--- game test/main.cpp
+++ game test/main.cpp
@@ -37,7 +37,7 @@ int main() {
         player.setBlock(7);
         Enemy eleventhGrader(20,15);
         cout << "you push over the 10th grader and he expodes when he hits the floor" << endl << "after you knock the 10th grader to the floor, a 11th grader aproches you. this is what he looks like:" << endl ;
-        eleventhGrader.cole();
+        eleventhGrader.judeviation();
         return_value = mainGame.attackLoop(player, eleventhGrader);
         if(return_value == 0){
             playing = false;
@@ -47,7 +47,7 @@ int main() {
         player.setBlock(7);
         Enemy twelthGrader(25,18);
         cout << "you strangle the 11th grader until he goes unconshus" << endl << "after you knock out the 11th grader, a twelth grader aproches you. this is what he looks like:" << endl ;
-        twelthGrader.cole();
+        twelthGrader.ratman();
         return_value = mainGame.attackLoop(player, twelthGrader);
         if(return_value == 0){
             playing = false;
@@ -57,7 +57,7 @@ int main() {
         player.setBlock(7);
         Enemy malisaMizel(25,25);
         cout << "you put the twelth grader in a head lock" << endl << "after you let the twelth grader go, malisa mizel aproches you. this is what she looks like:" << endl ;
-        malisaMizel.cole();
+        malisaMizel.dripmiz();
         return_value = mainGame.attackLoop(player, malisaMizel);
         if(return_value == 0){
             playing = false;
